3509835830622ccf52b96b1a55e06c5720c701c5
Shubhi Rajput <ShubhiRajput0107@gmail.com>
2026-08-16 00:16:51 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/33. Search in Rotated Sorted Array/Solution.java
+++ LeetCode/Java/Medium/33. Search in Rotated Sorted Array/Solution.java
@@ -2,17 +2,13 @@ class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
-
             if (nums[mid] == target) {
                 return mid;
             }
-
-            // Left half sorted hai
+            // Left half is sorted
             if (nums[left] <= nums[mid]) {
-
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
                 } else {
@@ -20,9 +16,8 @@ class Solution {
                 }
             }
 
-            // Right half sorted hai
+            // Right half is sorted
             else {
-
                 if (nums[mid] < target && target <= nums[right]) {
                     left = mid + 1;
                 } else {
