c3358894ee0ab915e2fc44031f6e96df6c750972
Shubhi Rajput <ShubhiRajput0107@gmail.com>
2026-08-16 11:59:53 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/1. Two Sum/Solution.java
+++ LeetCode/Java/Easy/1. Two Sum/Solution.java
@@ -1,6 +1,45 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int [] arr= new int[2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
+/*
+Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{left,right}; 
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};   //just means "no valid answer was found."
+    }
+}
+/*        Map<Integer, Integer>map=new HashMap<>(); //Map<key,value>
+        for(int i=0;i<nums.length;i++){
+            int complement=target-nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return ;
+    }
+}
+       /* int [] arr= new int[2];
         int sum=0;
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
@@ -14,4 +53,4 @@ class Solution {
         }
         return arr;
     } 
-}
+}*/
