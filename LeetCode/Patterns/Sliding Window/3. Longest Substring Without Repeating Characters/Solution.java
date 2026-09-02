5131a70e2b63844dd5959b9c7072c2d69497b219
Shubhi Rajput <ShubhiRajput0107@gmail.com>
2026-09-02 15:17:06 +0530

    Add Medium Java Solution for 3. Longest Substring Without Repeating Characters from LeetCode

--- LeetCode/Patterns/Sliding Window/3. Longest Substring Without Repeating Characters/Solution.java
+++ LeetCode/Patterns/Sliding Window/3. Longest Substring Without Repeating Characters/Solution.java
@@ -1,20 +1,6 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        int left = 0, maxLen = 0;
-        for (int right = 0; right < s.length(); right++) {
-            char c = s.charAt(right);
-            if (map.containsKey(c)) {
-                // move left to max(currentLeft, lastSeen+1) to avoid going backwards
-                left = Math.max(left, map.get(c) + 1);
-            }
-            map.put(c, right);
-            maxLen = Math.max(maxLen, right - left + 1);
-        }
-        return maxLen;
-    }
-}
-       /* Set<Character> set = new HashSet<>();
+                Set<Character> set = new HashSet<>();
         int left = 0, maxLen = 0;
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
@@ -27,4 +13,18 @@ class Solution {
         }
         return maxLen;
         }
-    }*/
+    }
+/*        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, maxLen = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            if (map.containsKey(c)) {
+                // move left to max(currentLeft, lastSeen+1) to avoid going backwards
+                left = Math.max(left, map.get(c) + 1);
+            }
+            map.put(c, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+}*/
