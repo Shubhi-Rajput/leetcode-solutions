fe83e7168848adce6ed049f5be232f95eb0e3572
Shubhi Rajput <ShubhiRajput0107@gmail.com>
2026-09-04 23:40:18 +0530

    Add Medium Java Solution for 204. Count Primes from LeetCode

--- LeetCode/Java/Medium/204. Count Primes/Solution.java
+++ LeetCode/Java/Medium/204. Count Primes/Solution.java
@@ -1,6 +1,6 @@
 class Solution {
      public boolean isPrime(int n){
-        if(n<=1) return false;
+/*        if(n<=1) return false;
         for(int i=2;i*i<=n;i++){
             if(n%i==0){
                 return false;
