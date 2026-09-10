2456a82619aedc704e1574265a5e9764e0718d28
Shubhi Rajput <ShubhiRajput0107@gmail.com>
2026-09-11 00:16:41 +0530

    Add Medium Java Solution for 4248. Count Commas in Range II from LeetCode

--- LeetCode/Java/Medium/4248. Count Commas in Range II/Solution.java
+++ LeetCode/Java/Medium/4248. Count Commas in Range II/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
     public long countCommas(long n) {
-        long ans = 0;
-        long start = 1000;
+        long ans=0;
+        long start=1000;
         while (start <= n) {
             ans += n - start + 1;
             start *= 1000;
