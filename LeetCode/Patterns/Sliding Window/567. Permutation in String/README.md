68870313f828c7a1a4e7da05a6afa0ad5d50bb7d
Shubhi Rajput <ShubhiRajput0107@gmail.com>
2026-09-11 23:06:03 +0530

    Add Medium Java Solution for 567. Permutation in String from LeetCode

--- LeetCode/Patterns/Sliding Window/567. Permutation in String/README.md
+++ LeetCode/Patterns/Sliding Window/567. Permutation in String/README.md
@@ -8,8 +8,8 @@
 Hash Table, Two Pointers, String, Sliding Window
 
 ### 🚀 Performance
-- **Runtime:** 6 ms
-- **Memory:** 43.8 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Sliding Window/567. Permutation in String/Solution.java
+++ LeetCode/Patterns/Sliding Window/567. Permutation in String/Solution.java
@@ -1,5 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        int k = s1.length();
+        int[] freq1 = new int[128];
+        int[] freq2 = new int[128];
+        // Frequency of s1
+        for (int i = 0; i < k; i++) {
+            freq1[s1.charAt(i)]++;
+        }
+        // First window of s2
+        for (int i = 0; i < k; i++) {
+            freq2[s2.charAt(i)]++;
+        }
+        if (Arrays.equals(freq1, freq2)) {
+            return true;
+        }
+        int l = 0;
+        int r = k;
+        while (r < s2.length()) {
+            // Add right character
+            freq2[s2.charAt(r)]++;
+            // Remove left character
+            freq2[s2.charAt(l)]--;
+            l++;
+            r++;
+            if (Arrays.equals(freq1, freq2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
 /*        char[] arr1 = s1.toCharArray();
         Arrays.sort(arr1);
         String target = new String(arr1);
@@ -15,48 +48,3 @@ class Solution {
         return false;
     }
 }*/
-        if (s1.length() > s2.length()) {
-            return false;
-        }
-
-        int k = s1.length();
-
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-
-        // Frequency of s1
-        for (int i = 0; i < k; i++) {
-            freq1[s1.charAt(i) - 'a']++;
-        }
-
-        // First window of s2
-        for (int i = 0; i < k; i++) {
-            freq2[s2.charAt(i) - 'a']++;
-        }
-
-        if (Arrays.equals(freq1, freq2)) {
-            return true;
-        }
-
-        int l = 0;
-        int r = k;
-
-        while (r < s2.length()) {
-
-            // Add right character
-            freq2[s2.charAt(r) - 'a']++;
-
-            // Remove left character
-            freq2[s2.charAt(l) - 'a']--;
-
-            l++;
-            r++;
-
-            if (Arrays.equals(freq1, freq2)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-}
